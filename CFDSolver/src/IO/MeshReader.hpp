c70130a92eb57b76c55d11bcc147db4b1d488488
AleBumbu <alexandrubumbu99@gmail.com>
2026-09-08 18:38:42 +0100

    Merge branch 'fileSystem' into demoOBJRendering

--- CFDSolver/src/IO/MeshReader.hpp
+++ CFDSolver/src/IO/MeshReader.hpp
@@ -1,10 +1,7 @@
 #pragma once
-#include <iostream>
-#include <fstream>
-#include <sstream>
+
 #include <string>
 #include <vector>
-#include <filesystem>
 
 namespace CFD {
 
@@ -15,11 +12,11 @@ namespace CFD {
         float z;
 
         ReadVertex(float x, float y, float z)
-            :x(x), y(y), z(z) {
+            : x(x), y(y), z(z) {
         }
 
         ReadVertex(const ReadVertex& vertex)
-            :x(vertex.x), y(vertex.y), z(vertex.z) {
+            : x(vertex.x), y(vertex.y), z(vertex.z) {
         }
     };
 
@@ -28,14 +25,14 @@ namespace CFD {
         float x;
         float y;
         float z;
+
         ReadNormal(float x, float y, float z)
-            :x(x), y(y), z(z) {
+            : x(x), y(y), z(z) {
         }
 
         ReadNormal(const ReadNormal& normal)
-            :x(normal.x), y(normal.y), z(normal.z) {
+            : x(normal.x), y(normal.y), z(normal.z) {
         }
-
     };
 
     struct FaceVertex
@@ -56,78 +53,6 @@ namespace CFD {
         std::vector<Face> faces;
     };
 
-    OBJMesh loadOBJ(const std::string& filepath) {
-        OBJMesh mesh;
-        std::cout << "mesh created!!!";
-
-        std::ifstream file(filepath);
-        if (std::filesystem::exists(filepath)) {
-            std::cout << "File exists!\n";
-        }
-        else {
-            std::cout << "File NOT found!\n";
-            std::cout << "Current dir: "
-                << std::filesystem::current_path()
-                << "\n";
-        }
-        std::string line;
-
-        while (std::getline(file, line)) {
-            std::istringstream stream(line);
-            std::string type;
-            stream >> type;
-
-            if (type.empty())
-                continue;
-
-            else if (type == "v") {
-                float x, y, z;
-                stream >> x >> y >> z;
-                mesh.vertices.emplace_back(x, y, z);
-            }
-
-            else if (type == "vn") {
-                float x, y, z;
-                stream >> x >> y >> z;
-
-                mesh.normals.emplace_back(x, y, z);
-            }
-
-            else if (type == "f") {
-                Face face;
-                std::string token;
-
-                while (stream >> token) {
-                    std::stringstream tokenStream(token);
-
-                    std::string viString;
-                    std::string textureIndexString;
-                    std::string vniString;
-
-                    std::getline(tokenStream, viString, '/');
-                    std::getline(tokenStream, textureIndexString, '/');
-                    std::getline(tokenStream, vniString, '/');
-
-                    int vi =
-                        std::stoi(viString) - 1;
-
-                    int vni = -1;
-
-                    if (!vniString.empty())
-                    {
-                        vni =
-                            std::stoi(vniString) - 1;
-                    }
-
-                    face.verticesFace.push_back({
-                        vi,
-                        vni
-                        });
-                }
-                mesh.faces.push_back(face);
-            }
-        }
-        return mesh;
-    }
+    OBJMesh loadOBJ(const std::string& filepath);
 
 } // namespace CFD
